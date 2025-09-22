988e0d783efdf5c8dd3f8e0e242639d67541dcec
JITHIN-Ji <148471709+JITHIN-Ji@users.noreply.github.com>
2025-09-22 14:28:55 +0530

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ console.log('Starting server on port:', PORT);
 const VERIFY_TOKEN = 'your_secure_verify_token_12345';
 
 // ADD YOUR PAGE ACCESS TOKEN HERE
-const PAGE_ACCESS_TOKEN = 'EAALkgHsGfroBPqdHm79ufBy5mfX6EpLzu0S2wBFTCUAqIibmvxDYD07zrsx8LEIZCr4VJmuvDnRdgGeX19Pf4CV3JZBtSFCsrdk8xJvnftwYNBSlHrjiHM5JAwhnweks99QgYDqU18QORD6M0N0vUqlP4Ow5CTxmnGSVU7PKjt7oQvZA3ERZA4QceIZCZC8cfMhIHctHl2eAZDZD'; // Replace with your actual token from Facebook
+const PAGE_ACCESS_TOKEN = 'EAALkgHsGfroBPnonkZAP3e8ZCOsK16dU9WHpFqM3eBkQVtor22jCJTqMLRacZCfCeZBoIZBVtR5EEBOusmDZBZCfYKZBYVf7vMguFfVeihYDUo7vKf3QHvKhSjRRmX5wCmmnbd5vxGJo5V0xi64KLICDTIZCgOBZBCIS3ZCojN2w1CjeJdXReOWTkZBZA4JhyXOtotcKlREhhvtNRbAZDZD'; // Replace with your actual token from Facebook
 
 app.use(bodyParser.json());
 
